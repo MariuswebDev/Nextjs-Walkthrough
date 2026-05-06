4600bfd4ce3ef06e44c783096f5b395ff4cddfc1
MariuswebDev <efuetlanchamarius@gmail.com>
2026-05-06 18:05:46 +0100

    modified2: portfolio site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "My Site",
@@ -14,6 +15,37 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-full flex flex-col bg-gray-900">
+        <nav className="bg-gray-700 px-3 flex p-6 justify-between text-white items-center top-0 sticky">
+          <span className="text-white font-bold text-3xl cursor-pointer">
+            Marius <span className="text-blue-400">Dev</span>
+          </span>
+          <div className="space-x-4 font-semibold cursor-pointer">
+            <Link
+              className={`hover:text-amber-600 transition-colors hover:font-bold`}
+              href="/"
+            >
+              Home
+            </Link>
+            <Link
+              className={`hover:text-amber-600 transition-colors hover:font-bold `}
+              href="/projects"
+            >
+              Projects
+            </Link>
+            <Link
+              className={`hover:text-amber-600 transition-colors hover:font-bold `}
+              href="/about"
+            >
+              Skills
+            </Link>
+            <Link
+              className={`hover:text-amber-600 transition-colors hover:font-bold `}
+              href="/contact"
+            >
+              Contact
+            </Link>
+          </div>
+        </nav>
         <main className="container mx-auto">{children}</main>
       </body>
     </html>
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,41 +1,9 @@
 // "use client";
 import Image from "next/image";
-import Link from "next/link";
 
 const Dashboard = () => {
   return (
     <div className="mx-auto text-white font-sans">
-      <nav className="bg-gray-700 px-3 flex p-6 justify-between items-center top-0 sticky">
-        <span className="text-white font-bold text-3xl cursor-pointer">
-          Marius <span className="text-blue-400">Dev</span>
-        </span>
-        <div className="space-x-4 font-semibold cursor-pointer">
-          <Link
-            className={`hover:text-amber-600 transition-colors hover:font-bold`}
-            href="/"
-          >
-            Home
-          </Link>
-          <Link
-            className={`hover:text-amber-600 transition-colors hover:font-bold `}
-            href="/projects"
-          >
-            Projects
-          </Link>
-          <Link
-            className={`hover:text-amber-600 transition-colors hover:font-bold `}
-            href="/about"
-          >
-            Skills
-          </Link>
-          <Link
-            className={`hover:text-amber-600 transition-colors hover:font-bold `}
-            href="/contact"
-          >
-            Contact
-          </Link>
-        </div>
-      </nav>
       <header className="bg-gray-900 border-b border-b-gray-400 flex flex-col gap-2 p-6 items-center">
         <Image
           src="https://avatars.githubusercontent.com/u/236823006?s=400&u=50b7982621ec24c5aacb47286de1156e148c2510&v=4"
